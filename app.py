e8787972fe1ab8f7e24d099d5f2b80b7ce7e9ab3
Phillip Salazar <phillipsalazar96@gmail.com>
2019-10-07 15:53:12 -0500

    added post and get functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # create the application object
 app = Flask(__name__)
@@ -10,9 +10,39 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/todolist')
+# @app.route('/todolist')
+# def todolist():
+#         return render_template('todolist.html')
+
+@app.route('/todolist',methods = ['POST', 'GET'])
 def todolist():
-    return render_template('todolist.html')
+   if request.method == 'POST':
+        delete = request.form.get('data')
+        if delete == '0':
+            result = request.form.get('name')
+            myfile = open("data.txt", "a")
+            myfile.write(result + '\n')
+            myfile.close()
+
+        elif delete == '1':
+            myfile = open("data.txt", "w").close()
+
+        myfile = open("data.txt", "r")
+        result = myfile.read()
+        result = result.split('\n')
+        return render_template("todolist.html",data = result)
+
+   elif request.method == 'GET':
+       myfile = open("data.txt", "r")
+       result = myfile.read()
+       result = result.split('\n')
+       return render_template("todolist.html",data = result)
+
+# @app.route('/result',methods = ['POST', 'GET'])
+# def result():
+#    if request.method == 'POST':
+#       result = request.form
+#       return render_template("data.html")
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
